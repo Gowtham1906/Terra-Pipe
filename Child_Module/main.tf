50037635ff75078b8d32d46e77389126352fe440
S.GOWTHAM <88620210+Gowtham1906@users.noreply.github.com>
2024-10-19 13:46:05 +0530

    Update main.tf

--- Child_Module/main.tf
+++ Child_Module/main.tf
@@ -14,11 +14,4 @@ terraform {
     region = "us-east-1" 
   }
 }
-#Updated main.tf
-resource "aws_vpc" "my_existing_vpc" {
-  for_each              = var.imported_vpc_configs
-  cidr_block            = each.value.cidr_block
-  enable_dns_support    = each.value.enable_dns_support
-  enable_dns_hostnames  = each.value.enable_dns_hostnames
-  tags                  = each.value.tags
-}
+
